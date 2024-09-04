c922698e3de31b528e012cf71a236c37ac40261f
Onat <73953584+du0ks@users.noreply.github.com>
2024-09-04 17:42:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Adding a blinking cursor effect at the footer
-    const cursor = document.createElement('span');
-    cursor.classList.add('blink');
-    cursor.textContent = '_';
-    const footer = document.querySelector('footer');
-    footer.appendChild(cursor);
-});
